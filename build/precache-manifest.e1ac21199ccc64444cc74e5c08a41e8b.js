bf7d848736d2e29d2c6bcafa7935ea79505237e8
Kamesh <kvedula@uci.edu>
2019-07-25 13:58:26 -0700

    Create a React portfolio

---
+++ build/precache-manifest.e1ac21199ccc64444cc74e5c08a41e8b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7808d4a0a7dcacf71d269a5c3a3087",
+    "url": "/Site/index.html"
+  },
+  {
+    "revision": "8ec830579f77c810cf0f",
+    "url": "/Site/static/css/main.4de1de90.chunk.css"
+  },
+  {
+    "revision": "06a00c1cb2c371230dfd",
+    "url": "/Site/static/js/2.aee06cf2.chunk.js"
+  },
+  {
+    "revision": "8ec830579f77c810cf0f",
+    "url": "/Site/static/js/main.129a942e.chunk.js"
+  },
+  {
+    "revision": "575779a7ea59d065ee61",
+    "url": "/Site/static/js/runtime~main.24bf1f1f.js"
+  },
+  {
+    "revision": "8d0060ef2b57b8b601911aea92f25b34",
+    "url": "/Site/static/media/christian.8d0060ef.jpg"
+  },
+  {
+    "revision": "fc76e730632fa6047006673fba9484e3",
+    "url": "/Site/static/media/photo-1.fc76e730.jpg"
+  },
+  {
+    "revision": "64af809139c2149c5672054f7acc6701",
+    "url": "/Site/static/media/photo-2.64af8091.jpg"
+  },
+  {
+    "revision": "32fb4cabbf89af1020130c45ab2bc15b",
+    "url": "/Site/static/media/photo-3.32fb4cab.jpg"
+  },
+  {
+    "revision": "dca6d3a3831f16b2f910b256a456ebd1",
+    "url": "/Site/static/media/photo-4.dca6d3a3.jpg"
+  },
+  {
+    "revision": "d2422a8207f40ac44344a1a51923b296",
+    "url": "/Site/static/media/photo-5.d2422a82.jpg"
+  },
+  {
+    "revision": "542a54f25aea4d2b6542b3363e6bbd13",
+    "url": "/Site/static/media/photo-6.542a54f2.jpg"
+  },
+  {
+    "revision": "d859506c935d4d843be297cf1111f37b",
+    "url": "/Site/static/media/photo-7.d859506c.jpg"
+  },
+  {
+    "revision": "4fa96ebfe9b24d46146194a8c19a0862",
+    "url": "/Site/static/media/photo-8.4fa96ebf.jpg"
+  },
+  {
+    "revision": "f4d41f15af4e149b4239641f503710ce",
+    "url": "/Site/static/media/profile-bg.f4d41f15.jpg"
+  },
+  {
+    "revision": "646f0bb7fda22f91cc5986cd04e45678",
+    "url": "/Site/static/media/work-1.646f0bb7.jpg"
+  },
+  {
+    "revision": "e5bcd802e4d1d794e8a9e450f4fdf8a5",
+    "url": "/Site/static/media/work-2.e5bcd802.jpg"
+  },
+  {
+    "revision": "28163de82e94481bd7efbe8db15b2e52",
+    "url": "/Site/static/media/work-3.28163de8.jpg"
+  },
+  {
+    "revision": "5ef5d00220be13ea39d287845525727e",
+    "url": "/Site/static/media/work-4.5ef5d002.jpg"
+  },
+  {
+    "revision": "3765093918b4afe098cef44c2997c90b",
+    "url": "/Site/static/media/work-5.37650939.jpg"
+  }
+]);
