a9fc80a16a9b3ea366de9104a7ffb23bf92e5438
Kamesh <kvedula@uci.edu>
2019-07-25 15:53:21 -0700

    add CNAME file and modify package

---
+++ build/precache-manifest.6da77606e4ebd2acf8accd4bd9b00026.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5365f26acc4c51b80df475b4e5fedf78",
+    "url": "kvedula.github.io/Site/index.html"
+  },
+  {
+    "revision": "cdeac99457bfc9b847c9",
+    "url": "kvedula.github.io/Site/static/css/main.4de1de90.chunk.css"
+  },
+  {
+    "revision": "32437429b12864ef7f31",
+    "url": "kvedula.github.io/Site/static/js/2.db1f296d.chunk.js"
+  },
+  {
+    "revision": "cdeac99457bfc9b847c9",
+    "url": "kvedula.github.io/Site/static/js/main.891fbdd2.chunk.js"
+  },
+  {
+    "revision": "538b3932b60e932b58fa",
+    "url": "kvedula.github.io/Site/static/js/runtime~main.149b3d5b.js"
+  },
+  {
+    "revision": "8d0060ef2b57b8b601911aea92f25b34",
+    "url": "kvedula.github.io/Site/static/media/christian.8d0060ef.jpg"
+  },
+  {
+    "revision": "fc76e730632fa6047006673fba9484e3",
+    "url": "kvedula.github.io/Site/static/media/photo-1.fc76e730.jpg"
+  },
+  {
+    "revision": "64af809139c2149c5672054f7acc6701",
+    "url": "kvedula.github.io/Site/static/media/photo-2.64af8091.jpg"
+  },
+  {
+    "revision": "32fb4cabbf89af1020130c45ab2bc15b",
+    "url": "kvedula.github.io/Site/static/media/photo-3.32fb4cab.jpg"
+  },
+  {
+    "revision": "dca6d3a3831f16b2f910b256a456ebd1",
+    "url": "kvedula.github.io/Site/static/media/photo-4.dca6d3a3.jpg"
+  },
+  {
+    "revision": "d2422a8207f40ac44344a1a51923b296",
+    "url": "kvedula.github.io/Site/static/media/photo-5.d2422a82.jpg"
+  },
+  {
+    "revision": "542a54f25aea4d2b6542b3363e6bbd13",
+    "url": "kvedula.github.io/Site/static/media/photo-6.542a54f2.jpg"
+  },
+  {
+    "revision": "d859506c935d4d843be297cf1111f37b",
+    "url": "kvedula.github.io/Site/static/media/photo-7.d859506c.jpg"
+  },
+  {
+    "revision": "4fa96ebfe9b24d46146194a8c19a0862",
+    "url": "kvedula.github.io/Site/static/media/photo-8.4fa96ebf.jpg"
+  },
+  {
+    "revision": "f4d41f15af4e149b4239641f503710ce",
+    "url": "kvedula.github.io/Site/static/media/profile-bg.f4d41f15.jpg"
+  },
+  {
+    "revision": "646f0bb7fda22f91cc5986cd04e45678",
+    "url": "kvedula.github.io/Site/static/media/work-1.646f0bb7.jpg"
+  },
+  {
+    "revision": "e5bcd802e4d1d794e8a9e450f4fdf8a5",
+    "url": "kvedula.github.io/Site/static/media/work-2.e5bcd802.jpg"
+  },
+  {
+    "revision": "28163de82e94481bd7efbe8db15b2e52",
+    "url": "kvedula.github.io/Site/static/media/work-3.28163de8.jpg"
+  },
+  {
+    "revision": "5ef5d00220be13ea39d287845525727e",
+    "url": "kvedula.github.io/Site/static/media/work-4.5ef5d002.jpg"
+  },
+  {
+    "revision": "3765093918b4afe098cef44c2997c90b",
+    "url": "kvedula.github.io/Site/static/media/work-5.37650939.jpg"
+  }
+]);
